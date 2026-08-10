7d41dd3a4ca49af2f4e125ece319dc7d3d45eb2b
0x1 <kevin.francart3@gmail.com>
2026-08-10 13:56:38 +0200

    misc

--- src/Node/Net/Server.go
+++ src/Node/Net/Server.go
@@ -1,26 +1,111 @@
 package main
+
 import (
-    "time"
-    Node_EventEmitter "gopurs/output/Node.EventEmitter"
-    "gopurs/output/gopurs_runtime"
+	"fmt"
+	"gopurs/output/Node.EventEmitter"
+	"gopurs/output/gopurs_runtime"
+	"net"
 )
 
-func NewServerImpl() interface{} { return Node_EventEmitter.NewImpl(nil) }
-func NewServerOptionsImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func AddressTcpImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func AddressIpcImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func CloseImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func GetConnectionsImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-
-func ListenImpl(arg0 interface{}, arg1 interface{}) interface{} {
-    go func() {
-        time.Sleep(50 * time.Millisecond)
-        Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(arg0), "listening", nil)
-    }()
-    return arg0
+func NewServerImpl() interface{} { 
+    return Node_EventEmitter.NewImpl(nil) 
 }
 
-func ListeningImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func MaxConnectionsImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func RefImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func UnrefImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
+func NewServerOptionsImpl(arg0 interface{}) interface{} { 
+    return Node_EventEmitter.NewImpl(nil) 
+}
+
+func ListenImpl(arg0 interface{}, arg1 interface{}) interface{} {
+	s := gopurs_runtime.Unbox[*Node_EventEmitter.EventEmitter](arg0)
+	
+	options := gopurs_runtime.RecordToMap(arg1.(gopurs_runtime.Value))
+	port := gopurs_runtime.Unbox[int64](options["port"])
+	
+	hostVal, hasHost := options["host"]
+	var host string
+	if hasHost {
+		host = gopurs_runtime.Unbox[string](hostVal)
+	} else {
+		host = "localhost"
+	}
+	
+	address := fmt.Sprintf("%s:%d", host, port)
+
+	go func() {
+		listener, err := net.Listen("tcp", address)
+		if err != nil {
+			Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(s), "error", gopurs_runtime.Box(err.Error()), nil)
+			return
+		}
+		
+		s.Any = listener
+		
+		Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(s), "listening", nil)
+		
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(s), "close", nil)
+				break
+			}
+			
+			// We need to create a new Socket object and emit "connection"
+			socketObj := Node_EventEmitter.NewImpl(nil).(*Node_EventEmitter.EventEmitter)
+			socketObj.Any = conn
+			
+			// Emit connection event with the socket
+			Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(s), "connection", gopurs_runtime.Box(socketObj), nil)
+			
+			// start read loop for the accepted socket
+			go func(c net.Conn, sock *Node_EventEmitter.EventEmitter) {
+				if sock.Any == nil {
+					return // HTTP Server stole the socket, don't read from it!
+				}
+				buf := make([]byte, 8192)
+				for {
+					n, err := c.Read(buf)
+					if n > 0 {
+						data := make([]byte, n)
+						copy(data, buf[:n])
+						Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(sock), "data", gopurs_runtime.Box(data), nil)
+					}
+					if err != nil {
+						Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(sock), "end", nil)
+						Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(sock), "close", gopurs_runtime.Box(false), nil)
+						break
+					}
+				}
+			}(conn, socketObj)
+		}
+	}()
+	return nil
+}
+
+func CloseImpl(arg0 interface{}) interface{} {
+    s := gopurs_runtime.Unbox[*Node_EventEmitter.EventEmitter](arg0)
+    if listener, ok := s.Any.(net.Listener); ok {
+        listener.Close()
+    }
+    return nil
+}
+
+func AddressTcpImpl(arg0 interface{}) interface{} {
+    s := gopurs_runtime.Unbox[*Node_EventEmitter.EventEmitter](arg0)
+    if listener, ok := s.Any.(net.Listener); ok {
+        if addr, ok2 := listener.Addr().(*net.TCPAddr); ok2 {
+            rec := make(map[string]gopurs_runtime.Value)
+            rec["port"] = gopurs_runtime.Box(int64(addr.Port))
+            rec["family"] = gopurs_runtime.Box("IPv4")
+            rec["address"] = gopurs_runtime.Box(addr.IP.String())
+            return rec
+        }
+    }
+    return nil 
+}
+
+func AddressIpcImpl(arg0 interface{}) interface{} { return nil }
+func GetConnectionsImpl(arg0 interface{}, arg1 interface{}) interface{} { return nil }
+func ListeningImpl(arg0 interface{}) interface{} { return true }
+func MaxConnectionsImpl(arg0 interface{}) interface{} { return 0 }
+func RefImpl(arg0 interface{}) interface{} { return nil }
+func UnrefImpl(arg0 interface{}) interface{} { return nil }
--- src/Node/Net/Socket.go
+++ src/Node/Net/Socket.go
@@ -1,31 +1,94 @@
 package main
-import Node_EventEmitter "gopurs/output/Node.EventEmitter"
 
-func NewImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
+import (
+	"fmt"
+	"gopurs/output/Node.EventEmitter"
+	"gopurs/output/gopurs_runtime"
+	"net"
+)
+
+func NewImpl(arg0 interface{}) interface{} {
+	e := Node_EventEmitter.NewImpl(nil).(*Node_EventEmitter.EventEmitter)
+	return e
+}
+
+func ConnectTcpImpl(arg0 interface{}, arg1 interface{}) interface{} {
+	s := gopurs_runtime.Unbox[*Node_EventEmitter.EventEmitter](arg0)
+	
+	options := gopurs_runtime.RecordToMap(arg1.(gopurs_runtime.Value))
+	port := gopurs_runtime.Unbox[int64](options["port"])
+	
+	hostVal, hasHost := options["host"]
+	var host string
+	if hasHost {
+		host = gopurs_runtime.Unbox[string](hostVal)
+	} else {
+		host = "localhost"
+	}
+	address := fmt.Sprintf("%s:%d", host, port)
+
+	go func() {
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(s), "error", gopurs_runtime.Box(err.Error()), nil)
+			return
+		}
+		
+		s.Any = conn // Set io.Writer / io.Closer for Node.Stream
+		
+		Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(s), "connect", nil)
+		Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(s), "ready", nil)
+		
+		buf := make([]byte, 8192)
+		for {
+			n, err := conn.Read(buf)
+			if n > 0 {
+				data := make([]byte, n)
+				copy(data, buf[:n])
+				Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(s), "data", gopurs_runtime.Box(data), nil)
+			}
+			if err != nil {
+				Node_EventEmitter.GopursUnsafeEmitFn1(gopurs_runtime.Box(s), "end", nil)
+				Node_EventEmitter.GopursUnsafeEmitFn2(gopurs_runtime.Box(s), "close", gopurs_runtime.Box(false), nil)
+				break
+			}
+		}
+	}()
+	return arg0
+}
+
+func CreateConnectionImpl(arg0 interface{}) interface{} {
+	s := NewImpl(nil)
+	return ConnectTcpImpl(s, arg0)
+}
+
 func AddressImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
 func BytesReadImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
 func BytesWrittenImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func CreateConnectionImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func ConnectTcpImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func ConnectIpcImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func ConnectingImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func DestroySoonImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func LocalAddressImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func LocalPortImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func LocalFamilyImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func PendingImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func RefImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func RemoteAddressImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func RemotePortImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func RemoteFamilyImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func ResetAndDestroyImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetKeepAliveImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetKeepAliveBooleanImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetKeepAliveInitialDelayImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetKeepAliveAllImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetNoDelayImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetNoDelayBooleanImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func SetTimeoutImpl(arg0 interface{}, arg1 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func TimeoutImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func UnrefImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
-func ReadyStateImpl(arg0 interface{}) interface{} { return Node_EventEmitter.NewImpl(nil) }
+func ConnectIpcImpl(arg0 interface{}, arg1 interface{}) interface{} { return arg0 }
+func ConnectingImpl(arg0 interface{}) interface{} { return false }
+func DestroySoonImpl(arg0 interface{}) interface{} { 
+    if s, ok := gopurs_runtime.Unbox[*Node_EventEmitter.EventEmitter](arg0).Any.(net.Conn); ok {
+        s.Close()
+    }
+    return nil 
+}
+func LocalAddressImpl(arg0 interface{}) interface{} { return "" }
+func LocalPortImpl(arg0 interface{}) interface{} { return 0 }
+func LocalFamilyImpl(arg0 interface{}) interface{} { return "" }
+func PendingImpl(arg0 interface{}) interface{} { return false }
+func RefImpl(arg0 interface{}) interface{} { return nil }
+func RemoteAddressImpl(arg0 interface{}) interface{} { return "" }
+func RemotePortImpl(arg0 interface{}) interface{} { return 0 }
+func RemoteFamilyImpl(arg0 interface{}) interface{} { return "" }
+func ResetAndDestroyImpl(arg0 interface{}) interface{} { return nil }
+func SetKeepAliveImpl(arg0 interface{}) interface{} { return nil }
+func SetKeepAliveBooleanImpl(arg0 interface{}, arg1 interface{}) interface{} { return nil }
+func SetKeepAliveInitialDelayImpl(arg0 interface{}, arg1 interface{}) interface{} { return nil }
+func SetKeepAliveAllImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { return nil }
+func SetNoDelayImpl(arg0 interface{}) interface{} { return nil }
+func SetNoDelayBooleanImpl(arg0 interface{}, arg1 interface{}) interface{} { return nil }
+func SetTimeoutImpl(arg0 interface{}, arg1 interface{}) interface{} { return nil }
+func TimeoutImpl(arg0 interface{}) interface{} { return nil }
+func UnrefImpl(arg0 interface{}) interface{} { return nil }
+func ReadyStateImpl(arg0 interface{}) interface{} { return "open" }
